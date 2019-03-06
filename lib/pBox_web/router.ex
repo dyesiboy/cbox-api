0013c69a4aaf95927f62f84b918b42718f8f849b
Jessie Arevalo <dt.jessie7@gmail.com>
2019-03-07 06:07:08 +0800

    Added guardian serializer

--- lib/pBox_web/router.ex
+++ lib/pBox_web/router.ex
@@ -3,9 +3,30 @@ defmodule PBoxWeb.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+    plug Guardian.Plug.VerifyHeader, realm: "Bearer"
+    plug Guardian.Plug.LoadResource
+  end
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
   end
 
   scope "/api", PBoxWeb do
     pipe_through :api
+
+    post "/sessions", SessionController, :create
+    delete "/sessions", SessionController, :delete
+    post "/sessions/refresh", SessionController, :refresh
+    resources "/users", UserController, only: [:create]
+  end
+
+  scope "/", PBox do
+    pipe_through :browser
+
+    get "/", PageController, :index
   end
 end
